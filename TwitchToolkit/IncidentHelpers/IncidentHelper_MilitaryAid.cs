0c3e79d3a8ed0d54c5e52790c6093ada9ac094ba
lbmaian <maian330@gmail.com>
2020-02-07 23:34:45 -0800

    Attempt fix for military aid incident when no post-medieval factions

--- TwitchToolkit/IncidentHelpers/IncidentHelper_MilitaryAid.cs
+++ TwitchToolkit/IncidentHelpers/IncidentHelper_MilitaryAid.cs
@@ -1,8 +1,5 @@
-﻿using RimWorld;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
+using RimWorld;
 using TwitchToolkit.Incidents;
 using TwitchToolkit.Store;
 using Verse;
@@ -13,33 +10,46 @@ namespace TwitchToolkit.IncidentHelpers.MilitaryAid
     {
         public override bool IsPossible()
         {
-            return true;
+            return TryFindAlly() != null;
         }
 
         public override void TryExecute()
         {
             var incident = new IncidentWorker_CallForAid();
-            
+
+            var tryAlly = TryFindAlly();
+
+            IncidentParms incidentParms = new IncidentParms();
+            incidentParms.target = Helper.AnyPlayerMap;
+            incidentParms.faction = tryAlly;
+            if (tryAlly.def.techLevel >= TechLevel.Industrial)
+                incidentParms.raidArrivalModeForQuickMilitaryAid = true;
+            incidentParms.points = DiplomacyTuning.RequestedMilitaryAidPointsRange.RandomInRange;
+            tryAlly.lastMilitaryAidRequestTick = Find.TickManager.TicksGame;
+            incident.TryExecute(incidentParms);
+        }
+
+        private Faction TryFindAlly()
+        {
             FactionManager manager = Find.FactionManager;
 
-            Faction ofPlayer = Faction.OfPlayer;
-            
             Faction tryAlly = manager.RandomAlliedFaction(false, false, true, TechLevel.Industrial);
 
             if (tryAlly == null)
             {
                 (from x in manager.AllFactions
-                where !x.IsPlayer && (false || !x.def.hidden) && (false || !x.defeated) && (true || x.def.humanlikeFaction) && (x.def.techLevel >= TechLevel.Industrial) && x.PlayerRelationKind == FactionRelationKind.Neutral
-                select x).TryRandomElement(out tryAlly);
+                 where !x.IsPlayer && !x.def.hidden && !x.defeated && x.def.techLevel >= TechLevel.Industrial && x.PlayerRelationKind == FactionRelationKind.Neutral
+                 select x).TryRandomElement(out tryAlly);
             }
-            
-			IncidentParms incidentParms = new IncidentParms();
-			incidentParms.target = Helper.AnyPlayerMap;
-			incidentParms.faction = tryAlly;
-			incidentParms.raidArrivalModeForQuickMilitaryAid = true;
-			incidentParms.points = DiplomacyTuning.RequestedMilitaryAidPointsRange.RandomInRange;
-			tryAlly.lastMilitaryAidRequestTick = Find.TickManager.TicksGame;
-            incident.TryExecute(incidentParms);
+
+            if (tryAlly == null)
+            {
+                (from x in manager.AllFactions
+                 where !x.IsPlayer && !x.def.hidden && !x.defeated && x.PlayerRelationKind == FactionRelationKind.Neutral
+                 select x).TryRandomElement(out tryAlly);
+            }
+
+            return tryAlly;
         }
     }
 }
